79061772b76f85d7cde3bfa3292fca37a82cfbb9
Jorge Rodriguez <jorge.rodriguez@webmaps.com.mx>
2018-04-25 18:57:11 -0500

    Se crea un RadioButton con estilos y su listener

--- app/src/main/java/mx/com/webmaps/md_ejercicio22/MainActivity.java
+++ app/src/main/java/mx/com/webmaps/md_ejercicio22/MainActivity.java
@@ -2,12 +2,33 @@ package mx.com.webmaps.md_ejercicio22;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    AppCompatCheckBox radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioButton = (AppCompatCheckBox)findViewById(R.id.radioButton1_id);
+
+        radioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+                    Toast.makeText(getApplicationContext(),"RadioButton is Enable ",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"RadioButton is Disable ",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
