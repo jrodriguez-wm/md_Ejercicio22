58f3c6db7674cabb727a0908bf1912637ffea784
Jorge Rodriguez <jorge.rodriguez@webmaps.com.mx>
2018-04-25 19:01:03 -0500

    Se crea un RadioGroup con su listener para diferenciar entre Radios

--- app/src/main/java/mx/com/webmaps/md_ejercicio22/MainActivity.java
+++ app/src/main/java/mx/com/webmaps/md_ejercicio22/MainActivity.java
@@ -4,28 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatCheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    AppCompatCheckBox radioButton;
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        radioButton = (AppCompatCheckBox)findViewById(R.id.radioButton1_id);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup_id);
 
-        radioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-                if(isChecked){
-                    Toast.makeText(getApplicationContext(),"RadioButton is Enable ",Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(getApplicationContext(),"RadioButton is Disable ",Toast.LENGTH_SHORT).show();
+                switch(checkedId){
+                    case R.id.radioButton1_id:{
+                        Toast.makeText(getApplicationContext(),"RadioButton1 is checked",Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case R.id.radioButton2_id:{
+                        Toast.makeText(getApplicationContext(),"RadioButton2 is checked",Toast.LENGTH_SHORT).show();
+                        break;
+                    }
                 }
             }
         });
